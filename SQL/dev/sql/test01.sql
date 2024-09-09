cfe3d36ef4743092c1fa0633c759874e209bb72a
Sangbeom Lee <sb486868@gmail.com>
2024-09-09 17:27:42 +0900

    Add files via upload

---
+++ SQL/dev/sql/test01.sql
@@ -0,0 +1,11 @@
+CREATE TABLE police_station
+(
+    name VARCHAR(16),
+    address VARCHAR(128)
+);
+
+INSERT INTO police_station VALUES 
+('서울특별시경찰청', '서울시 종로구 사직로길31'),
+('서울중부경찰서', '서울특별시 중구 수표로 27'), 
+('서울종로경찰서', '서울특별시 종로구 율곡로 46'), 
+('서울남대문경찰서', '서울특별시 중구 한강대로 410');
---
+++ SQL/dev/sql/test04.sql
@@ -0,0 +1,4 @@
+INSERT INTO sql_file VALUE (1,"test01.sql");
+INSERT INTO sql_file VALUE (2,"test02.sql");
+INSERT INTO sql_file VALUE (3,"test03.sql");
+INSERT INTO sql_file VALUE (4,"test04.sql");
